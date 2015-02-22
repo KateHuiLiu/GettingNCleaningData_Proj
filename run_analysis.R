ac31aca2b00aa0112ac60d15372b41de8e056c66
Kate Liu <hui.kate.liu@gmail.com>
2015-02-22 15:37:38 -0800

    Update code with the data label and merge
     with '#' will be ignored, and an empty message aborts the commit.

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 features <- read.table('UCI HAR Dataset/features.txt')
-keptfeatures<-features[grepl("mean()",features$V2,fixed = TRUE)|grepl("std()",features$V2,fixed = TRUE),]
+kept_features<-features[grepl("mean()",features[, 2],fixed = TRUE)|grepl("std()",features[, 2],fixed = TRUE),]
 
 train_X<-read.table("UCI HAR Dataset/train/X_train.txt")
 train_Y<-read.table("UCI HAR Dataset/train/Y_train.txt")
@@ -10,9 +10,34 @@ test_X<-read.table("UCI HAR Dataset/test/X_test.txt")
 test_Y<-read.table("UCI HAR Dataset/test/Y_test.txt")
 test_subject<-read.table("UCI HAR Dataset/test/subject_test.txt")
 
-# Merges the training and the test sets to create one data set.
-
 # Uses descriptive activity names to name the activities in the data set
+activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
+
+getactivitylabel<-function(activity_data, activity_labels){
+  activity_data<-cbind(activity_data, c(1:nrow(activity_data)))
+  activity_label_list<-merge(activity_data, activity_labels, by = "V1", All.x=TRUE, sort=FALSE)
+  names(activity_label_list)<-c("ActivityId","Index","Activity")
+  activity_label_list<- activity_label_list[order(activity_label_list$Index),]
+  return(activity_label_list)
+}
+
+train_Y_labeled<-getactivitylabel(train_Y,activity_labels)
+test_Y_labeled<-getactivitylabel(test_Y,activity_labels)
+
+# Merges the training and the test sets to create one data set.
+mergedata<-function(dataset, kept_features, subject, activity, type){
+  output<-matrix()
+  #only retrieve mean and std columns
+  dataset<-dataset[,kept_features[,2]]
+  output<-cbind(dataset,activity[,"Activity"])
+  output<-cbind(output, subject)
+  output<-cbind(output, type)
+  names(output)<-c(as.character(kept_features[, 2]),"Activity","Subject","Type")
+  return(output)
+}
+train_data<-mergedata(train_X,kept_features,train_subject,train_Y_labeled,"train")
+test_data<-mergedata(test_X,kept_features,test_subject,test_Y_labeled,"test")
+combined_set<-rbind(train_data,test_data)
 
 # Appropriately labels the data set with descriptive variable names. 
 headers<-names(combined_set)
